44c5022ebc58c8d4eece5bb28ff972107327fbbf
Manvithserugar <manvithserugar@gmail.com>
2025-02-01 19:53:28 +0530

    added basename to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import View from "./pages/View";
 import Manage from "./pages/Manage";
@@ -51,12 +51,17 @@ function App() {
         </Toolbar>
       </AppBar>
       <Container maxWidth="lg" sx={{ mt: 4 }}>
-        <Routes>
-          <Route path="/" element={<Home baseURL={config.baseURL} />} />
-          <Route path="/view" element={<View baseURL={config.baseURL} />} />
-          <Route path="/manage" element={<Manage baseURL={config.baseURL} />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
+        <BrowserRouter basename="/AttendanceApp-UI/">
+          <Routes>
+            <Route path="/" element={<Home baseURL={config.baseURL} />} />
+            <Route path="/view" element={<View baseURL={config.baseURL} />} />
+            <Route
+              path="/manage"
+              element={<Manage baseURL={config.baseURL} />}
+            />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </BrowserRouter>
       </Container>
     </div>
   );
